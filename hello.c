c72a2660b515ddc0c1cbd6c13e6d65c770042d88
Taras Bondarenko <tarasbondarenko2005@gmail.com>
2024-11-07 20:46:57 +0000

    Added lab4

---
+++ hello.c
@@ -0,0 +1,91 @@
+
+/*
+ * Copyright (c) 2017, GlobalLogic Ukraine LLC
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. All advertising materials mentioning features or use of this software
+ *    must display the following acknowledgement:
+ *    This product includes software developed by the GlobalLogic.
+ * 4. Neither the name of the GlobalLogic nor the
+ *    names of its contributors may be used to endorse or promote products
+ *    derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY GLOBALLOGIC UKRAINE LLC ``AS IS`` AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL GLOBALLOGIC UKRAINE LLC BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/printk.h>
+#include <linux/ktime.h>
+#include <linux/slab.h>
+#include <linux/list.h>
+
+MODULE_AUTHOR("IO-24 Bondarenko Taras");
+MODULE_DESCRIPTION("Hello, world in Linux Kernel Training");
+MODULE_LICENSE("Dual BSD/GPL");
+
+static uint count = 1;
+module_param(count, uint, 0444);
+MODULE_PARM_DESC(count, "The number of times to display - Hello World!");
+
+struct some_data {
+	struct list_head list;
+	ktime_t time;
+};
+
+static LIST_HEAD(list_head);
+
+static int __init hello_init(void)
+{
+	if (count == -2) {
+		printk(KERN_WARNING "(WARNING) - print count is -2");
+	} else if (count >= 3 && count <= 10) {
+		printk(KERN_WARNING "(WARNING) - print count is benween 3 and 10");
+	} else if (count > 8) {
+		printk(KERN_ERR "(ERROR) - print count is grater than 8");
+		return -EINVAL;
+	}
+
+	int i;
+	struct some_data *ptr;
+
+	for (i = 0; i < count; i++) {
+		ptr = kmalloc(sizeof(*ptr), GFP_KERNEL);
+		
+		if (ptr) {
+			ptr->time = ktime_get();
+			list_add_tail(&ptr->list, &list_head);
+		}
+		printk(KERN_EMERG "Hello, world!\n");
+	}
+	return 0;
+}
+
+static void __exit hello_exit(void)
+{
+	struct some_data *ptr, *tmp;
+	list_for_each_entry_safe(ptr, tmp, &list_head, list) {
+		printk(KERN_INFO "Time: %lld ns\n", ktime_to_ns(ptr->time));
+		list_del(&ptr->list);
+		kfree(ptr);
+	}
+}
+
+module_init(hello_init);
+module_exit(hello_exit);
